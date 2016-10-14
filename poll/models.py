d7638c1620c37531c8393f30101a1fc01e2c0005
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-14 12:00:37 +0300

    add the models to the poll app

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class QuestionGroup(models.Model):
+    heading = models.TextField()
+    text = models.TextField(blank=True)
+
+
+class Question(models.Model):
+    text = models.TextField()
+    question_group = models.ForeignKey(QuestionGroup)
+
+
+class Choice(models.Model):
+    text = models.TextField()
+    question = models.ForeignKey(Question)
+
+
+class Session(models.Model):
+    name = models.TextField(blank=True)
+    ip = models.CharField(max_length=200)
+    time_submitted = models.DateTimeField(auto_now=True)
+
+
+class Response(models.Model):
+    choice = models.ForeignKey(Choice)
+    session = models.ForeignKey(Session)
+    value = models.IntegerField()
+
