89e2abc0fac31080e4bbee6b5198f3a6f245302e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-11-09 15:30:19 +0200

    register models with admin

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from poll.models import *
 
-# Register your models here.
+admin.site.register(QuestionGroup)
+admin.site.register(Question)
+admin.site.register(Choice)
+
