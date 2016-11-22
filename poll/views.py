3a3fbb87375d26ffc4a859f89645686ed0334ba6
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-11-22 15:58:54 +0200

    logic to submit answers and get scores back

--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,56 @@
 import json
+from django.utils import timezone
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET, require_POST
 from poll import models
+
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_session(request):
+    ip = get_ip(request)
+    unclosed_sessions = models.Session.objects.filter(ip=ip, date_submitted__isnull=True)
+    if unclosed_sessions:
+        session = unclosed_sessions.get()
+    else:
+        session = models.Session(ip=ip, date_added=timezone.now())
+    return session
+
+def finish_session(request, responses):
+    session = get_session(request)
+    session.date_submitted = timezone.now()
+    session.save()
+    for ans in responses:
+        choice_id = ans['id']
+        choice_value = ans['value']
+        response = models.Response(
+            choice_id=choice_id,
+            value=choice_value,
+            session=session,
+        )
+        response.save()
+    return session
+
+def calculate_scores(session):
+    options = ["A", "B", "C", "D"]
+    num_ans = {o: 0 for o in options}
+    sum_ans = {o: 0.0 for o in options}
+
+    for answer in session.response_set.all():
+        option = answer.choice.option
+        num_ans[option] += 1
+        sum_ans[option] += answer.value
+
+    for o in options:
+        if num_ans[o] == 0:
+            num_ans[o] = 1
+
+    return {o: sum_ans[o] / num_ans[o] for o in options}
 
 @require_GET
 def questions(request):
@@ -32,15 +81,22 @@ def questions(request):
         }
         for g in models.QuestionGroup.objects.all()
     ]
+
+    get_session(request)
+
     return JsonResponse({"status": "ok", "groups": groups})
 
 @require_POST
 def submit(request):
     data = json.loads(request.body.decode("utf-8"))
-    print(data)
+    responses = data['responses']
+
+    session = finish_session(request, responses)
+    scores = calculate_scores(session)
+    print(scores)
     return JsonResponse({
-        "status": "error",
-        "reason": "not implemented",
+        "status": "ok",
+        "scores": scores,
     })
 
 @require_GET
